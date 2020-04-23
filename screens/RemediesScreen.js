50be13e83f991b67eb543c507e5313976afd9639
srajan-17 <siddharthrajan10@gmail.com>
2020-04-23 18:50:19 -0400

    add nonfunctional dropdown menu

--- screens/RemediesScreen.js
+++ screens/RemediesScreen.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, ActivityIndicator, ScrollView } from 'react-native';
 import { useFirestoreDoc } from '../utils/db';
+import ModalDropdown from 'react-native-modal-dropdown';
 
 export default function RemediesScreen({ navigation, route }) {
-    
-    // query for the condition that was identified with the endpoint
-    // this needs to be updated so that 
-    const { isLoading, data } = useFirestoreDoc('conditions', 'acne');
+
+    const { isLoading, data } = useFirestoreDoc('conditions', "acne");
+
+    const conditions = ["acne", "eczema", "light disease", "melanoma", "nail fungus",
+                    "psoriasis", "scabies", "seborrheic keratoses", "ringworm", "warts"]
 
     return (
         <React.Fragment>
@@ -17,7 +19,20 @@ export default function RemediesScreen({ navigation, route }) {
             }} 
             contentContainerStyle={styles.container}
             >
-                <Text style={{fontWeight: "bold"}}> Symptoms: </Text>
+                <Text style={styles.title}>Common Conditions and Remedies</Text>
+                
+                <ModalDropdown 
+                    options={['Acne', 'Eczema', 'Light Disease', 'Melanoma', 
+                    'Nail Fungus', 'Psoriasis', 'Scabies', 'Keratoses', 'Ringworm', 'Warts']} 
+                    onSelect={value => {
+                        console.log(conditions[value])
+                        selectCondition(conditions[value])
+                        }}
+                />
+
+                <Text>{'\n'}</Text>
+
+                <Text style={styles.sectionTitles}> Symptoms </Text>
                 {data.symptoms.map((x, i) => {
                     return (
                     <Text key={i}>{i + 1}: {x}</Text>
@@ -26,7 +41,7 @@ export default function RemediesScreen({ navigation, route }) {
                 
                 <Text>{'\n'}</Text>
                 
-                <Text style={{fontWeight: "bold"}}> Home Remedies: </Text>
+                <Text style={styles.sectionTitles}> Home Remedies </Text>
                 {data.Home_Remedies.map((x, i) => {
                     return (
                     <Text key={i}>{i + 1}: {x}</Text>
@@ -34,17 +49,17 @@ export default function RemediesScreen({ navigation, route }) {
                 })}                
                     <Text>{'\n'}</Text>
                     
-                    <Text style={{fontWeight: "bold"}}> Tips: </Text>
+                    <Text style={styles.sectionTitles}> Tips </Text>
                     <Text> {data.tips} </Text>
 
                     <Text>{'\n'}</Text>
                     
-                    <Text style={{fontWeight: "bold"}}> Prevalence: </Text>
+                    <Text style={styles.sectionTitles}> Prevalence </Text>
                     <Text> {data.prevalence} </Text>
 
                     <Text>{'\n'}</Text>
                     
-                    <Text style={{fontWeight: "bold"}}> Nearest Doctor: </Text>
+                    <Text style={styles.sectionTitles}> Nearest Doctor </Text>
                     <Text> {data.doctor} </Text>        
             </ScrollView>
             }
@@ -55,9 +70,26 @@ export default function RemediesScreen({ navigation, route }) {
 const styles = StyleSheet.create({
     container: {
         justifyContent: "center",
-        padding: 20, backgroundColor: "#fefefe",
+        padding: 20,
+        paddingTop: 30, 
+        backgroundColor: "#fefefe"
     },
+    sectionTitles: {
+        fontWeight: "bold",
+        fontSize: 16,
+        textAlign: "center"
+    },
+    title: {
+        fontSize: 25,
+        textAlign: "center",
+        fontWeight: "bold",
+        textDecorationLine: "underline"
+    }
 })
+
+function selectCondition(input) {
+    //const { isLoading, data } = useFirestoreDoc('conditions', input);
+}
 
 function Loading() {
     return (
