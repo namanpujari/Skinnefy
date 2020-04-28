1bd83298dc434ed6e8469262d3ae096b8c24fb2a
namanpujari <namanpujari913@gmail.com>
2020-04-27 22:52:44 -0400

    clean up bar graph and implement blob

--- screens/ResultsScreen.js
+++ screens/ResultsScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Image, Text, StyleSheet, View, TouchableOpacity, ActivityIndicator, Dimensions } from 'react-native';
+import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, ActivityIndicator, Dimensions } from 'react-native';
 import Ionicons from '@expo/vector-icons';
 import { BarChart, } from 'react-native-chart-kit';
 
@@ -9,17 +9,17 @@ export default function ResultsScreen({ navigation, route }) {
     const { isLoading, data, err } = useEndpoint(route.params.data);
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={{padding: 10, flexDirection: "row", justifyContent: "center",}}>
                 <Image 
-                style={{ width: 210, height: 280, borderRadius: 10,  borderColor: "#333333", borderWidth: 2, }}
+                style={{ width: 150, height: 200, borderRadius: 10,  borderColor: "#333333", borderWidth: 2, }}
                 source={
                     { uri: route.params.data.uri }
                 }
                 />
             </View>
             <View style={styles.resultsContainer}>
-                <Text style={{color: "#666666", fontSize: 15}}> 
+                <Text style={{color: "#666666", fontSize: 14, marginBottom: 20,}}> 
                     According to our AI powered prediction engine these are your results
                 </Text>
                 { 
@@ -31,13 +31,26 @@ export default function ResultsScreen({ navigation, route }) {
 
                 {
                     !isLoading &&
-                    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                    <View style={{flex: 1, justifyContent: "center", alignItems: "center",}}>
                         {err ? 
                             <Text style={{color: "red"}}>ERROR: {err}</Text>
                         :
                             <BarChart
                                 // style={graphStyle}
-                                data={data}
+                                fromZero={true}
+                                segments={4}
+                                withInnerLines={false}
+                                data={{
+                                    labels: data.labels.slice(0, 2),
+                                    datasets: [
+                                        {
+                                            data: data.dataset.slice(0, 2).map(val => {
+                                                return val * 100;
+                                            }),
+                                        },
+                                    ],
+                                }}
+                                yAxisSuffix="%"
                                 width={Dimensions.get('window').width - 20}
                                 height={200}
                                 chartConfig={{
@@ -48,11 +61,14 @@ export default function ResultsScreen({ navigation, route }) {
                                     color: (opacity = 255) => `rgba(0, 0, 0, ${opacity})`,
                                     style: {
                                       borderRadius: 16
-                                    }
+                                    },
+                                
                                 }}                              
-                                bezier
                                 style={{
                                     borderRadius: 5,
+                                    padding: 5,
+                                    backgroundColor: "#999",
+                                    elevation: 5,
                                 }}                              
                             />
                         }
@@ -61,7 +77,7 @@ export default function ResultsScreen({ navigation, route }) {
                 }
 
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -71,7 +87,7 @@ const styles = StyleSheet.create({
     },
     resultsContainer: {
         flex: 1,
-        padding: 30,
+        padding: 20,
         backgroundColor: "#fefefe",
 
     }
--- utils/sagemakerCalls.js
+++ utils/sagemakerCalls.js
@@ -9,58 +9,90 @@ const endpoint_name = "skinnefy-training-version2-endpoint";
 const conditions = ["Acne", "Eczema", "Light Disease", "Melanoma", "Nail Fungus",
                     "Psoriasis", "Scabies", "Seborrheic Keratoses", "Ringworm", "Warts"];
 
+FileReader.prototype.readAsArrayBuffer = function (blob) {
+	if (this.readyState === this.LOADING) throw new Error("InvalidStateError");
+	this._setReadyState(this.LOADING);
+	this._result = null;
+	this._error = null;
+	const fr = new FileReader();
+	fr.onloadend = () => {
+        if(!fr.result) {
+            throw new Error("Image too big!")
+        }
+		const content = atob(fr.result.substr("data:application/octet-stream;base64,".length));
+		const buffer = new ArrayBuffer(content.length);
+		const view = new Uint8Array(buffer);
+		view.set(Array.from(content).map(c => c.charCodeAt(0)));
+		this._result = buffer;
+		this._setReadyState(this.DONE);
+	};
+	fr.readAsDataURL(blob);
+}
+
 export function useEndpoint(image) {
     const [queryState, setQueryState] = React.useState({ isLoading: true, data: null, err: null });
 
-    // convert the base64 encoding string to uint8 typearray
-    var result = convertToArray(image.base64);   
+    const sendAndRetrieve = async () => {
+        try {
+            const response = await fetch(image.uri);
+            const blob = await response.blob();
+            sagemakerruntime.invokeEndpoint({ 
+                Body: blob,
+                EndpointName: endpoint_name,
+                ContentType: "application/x-image",
+            }, function(err, data) {
+                if(err) {
+                    setQueryState({ 
+                        isLoading: false, 
+                        data: null, 
+                        err: err.message
+                    })
+                }
+                else {
+                    let prediction = prepareResponse(data.Body);
+                    setQueryState({ 
+                        isLoading: false, 
+                        data: prediction, 
+                        err: null
+                    })
+                }
+            });
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    }
+    // // convert the base64 encoding string to uint8 typearray
+    // var result = convertToArray(image.base64);      
     React.useEffect(() => {
-        sagemakerruntime.invokeEndpoint({
-            Body: result,
-            EndpointName: endpoint_name,
-            ContentType: "application/x-image", 
-        }, function(err, data) {
-            if(err) {
-                setQueryState({ 
-                    isLoading: false, 
-                    data: null, 
-                    err: err.message
-                })
-            }
-            else {
-                let prediction = prepareResponse(data.Body);
-                setQueryState({ 
-                    isLoading: false, 
-                    data: prediction, 
-                    err: null
-                })
-            }
-        });
+        sendAndRetrieve();
     }, []);
     return queryState;
 }
 
-function convertToArray(data) {
-    var raw = atob(data);
-    var rawLength = raw.length;
-    var array = new Uint8Array(new ArrayBuffer(rawLength));
-
-    for(var i = 0; i < rawLength; i++) {
-        array[i] = raw.charCodeAt(i);
-    }
-    
-    return array;
-}
-
 function prepareResponse(responseBuffer) {
-    const predictionValues = JSON.parse(responseBuffer.toString());    
+    var i, j;
+    const predictionValues = JSON.parse(responseBuffer.toString());   
+    console.log(predictionValues)
+    // manually sort two arrays. conditions based on predictions
+    for(i = 0; i < predictionValues.length; i++) {
+        for(j = i; j < predictionValues.length; j++) {
+            if(predictionValues[i] < predictionValues[j]) {
+                // swap first array
+                let tmp = predictionValues[i];
+                predictionValues[i] = predictionValues[j];
+                predictionValues[j] = tmp;
+                // swap second array
+                tmp = conditions[i];
+                conditions[i] = conditions[j];
+                conditions[j] = tmp;
+            }
+        }
+    }
+    console.log(predictionValues);
     const prediction = {
         labels: conditions,
-        datasets: [
-            {
-                data: predictionValues.map((x) => { return x * 100; }),
-            },
-        ],
+        dataset: predictionValues,
     }
     return prediction;
 }
